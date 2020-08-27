827aa6a1cd2217532cff0addb33e6316cf0aa793
ilaydacelenk <56550385+ilaydacelenk@users.noreply.github.com>
2020-08-27 21:33:47 +0300

    reactive

--- NYC_assignment/app.R
+++ NYC_assignment/app.R
@@ -31,13 +31,14 @@ ui <- fluidPage(
             ),
             selectInput("roomtype","Select Room Type",choices=data$room_type,selected = "Private room",multiple = TRUE),
           
+            actionButton("show_options", label = "Show Number of Options")
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel("Map", plotOutput("plot")),
+                tabPanel("Map", plotOutput("plot"), textOutput("options")),
                 tabPanel("Table", dataTableOutput("table")),
                 tabPanel("Number of Rooms", plotOutput("hist"))
             )
@@ -49,14 +50,22 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    rv_options <- eventReactive(input$show_options, {
+        paste("You have", data %>% filter(data$price>input$price[1] & data$price<input$price[2]) %>% summarise(count=n()), "options.")
+    })
+    
     output$table<- renderDataTable(data)
 
     output$plot <- renderPlot({
         dataplot <- data %>% filter(data$price>input$price[1] & data$price<input$price[2])
         
         ggplot(dataplot, aes(x=latitude, y=longitude)) + background_image(img) + geom_point(aes(color=room_type)) 
-
     })
+    
+    output$options <- renderText({
+        rv_options()
+    })
+    
     output$hist<- renderPlot({
         
         selected<- data%>%
