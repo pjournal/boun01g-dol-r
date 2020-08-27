c0c0b08d19a93178de9614fc214f94b9e7ffe0e1
ilaydacelenk <56550385+ilaydacelenk@users.noreply.github.com>
2020-08-27 19:06:50 +0300

    Create app.R

---
+++ NYC_assignment/AB_NYC_2019/app.R
@@ -0,0 +1,50 @@
+# Shiny app for Airbnb NYC 2019 Analysis
+
+library(shiny)
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(png)
+library(RCurl)
+library(jpeg)
+library(ggpubr)
+
+data <- read.csv("https://raw.githubusercontent.com/pjournal/boun01g-dol-r/gh-pages/NYC_assignment/AB_NYC_2019.csv", header = TRUE, check.names=TRUE)
+img <- readJPEG("/Users/ilaydacelenk/Desktop/projects/boun01g-dol-r/NYC_assignment/New_York_City_.png")
+
+
+ui <- fluidPage(
+
+    titlePanel("Find the Best Accommodation in NYC 2019"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("price",
+                        "Select a Price Range:",
+                        min = min(data$price),
+                        max = max(data$price),
+                        value = c(1000,2000),
+                        sep = ""
+            )
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("plot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$plot <- renderPlot({
+        dataplot <- data %>% filter(data$price>input$price[1] & data$price<input$price[2])
+        
+        ggplot(dataplot, aes(x=latitude, y=longitude)) + background_image(img) + geom_point(aes(color=room_type)) 
+
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
