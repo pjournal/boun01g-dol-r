cbdcb0a6b18af2951dfcbd6e6ac470062d62cb7c
ilaydacelenk <56550385+ilaydacelenk@users.noreply.github.com>
2020-09-06 23:44:25 +0300

    shiny uni

--- uni_exam_project/app.R
+++ uni_exam_project/app.R
@@ -4,6 +4,9 @@ library(readxl) # read_excel
 library(httr) # GET
 library(dplyr)
 library(ggplot2)
+library(shinyWidgets)
+library(reshape2)
+
 
 # Functions for manipulation
 mani97 <- function(data){
@@ -59,20 +62,68 @@ data2018 <- mani86(raw_df18)
 data2017 <- mani97(raw_df17)
 data2016 <- mani97(raw_df16)
 
+
+all_years_data<-bind_rows(mutate(data2016,year=2016),
+                          mutate(data2017,year=2017),
+                          mutate(data2018,year=2018),
+                          mutate(data2019,year=2019),
+                          mutate(data2020,year=2020))
+
+years <- sort(unique(all_years_data$year))
+unis <- sort(unique(all_years_data$university))
+depts <- sort(unique(all_years_data$department))
+cities <- sort(unique(all_years_data$city))
+
+
 ui <- fluidPage(
 
     titlePanel("University Entrance Exams"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("year",
-                        "Select a Year:",
-                        choices=c("2016", "2017", "2018", "2019", "2020"),
-                        selected = "2020")
+            pickerInput("year",
+                        "Select Years:",
+                        choices=years,
+                        selected=years, 
+                        multiple = TRUE, options = pickerOptions(actionsBox = TRUE) ),
+            pickerInput("uni", 
+                        "Select Universities:",
+                        choices=unis,
+                        selected=unis, 
+                        multiple = TRUE, options = pickerOptions(actionsBox = TRUE) ),
+            pickerInput("dept", 
+                        "Select Departments:",
+                        choices=depts,
+                        selected=depts, 
+                        multiple = TRUE, options = pickerOptions(actionsBox = TRUE) ),
+            pickerInput("city", 
+                        "Select Cities:",
+                        choices=cities,
+                        selected=cities, 
+                        multiple = TRUE, options = pickerOptions(actionsBox = TRUE) ),
+            radioButtons("ifcompare", "Do you want to compare universities for a department?",
+                         choices=c("YES", "NO"), selected = "YES"),
+            textOutput("selection_number"),
+            
+            pickerInput("uni5", 
+                        "Select at Most 5 Universities to Compare:",
+                        choices=unis,
+                        selected="BOĞAZİÇİ ÜNİVERSİTESİ", 
+                        multiple = TRUE, options = pickerOptions(actionsBox = TRUE, maxOptions=5) ),
+            pickerInput("dept1", 
+                        "Select a Department to Compare:",
+                        choices=depts,
+                        selected="Endüstri Mühendisliği (İngilizce)", 
+                        multiple = FALSE, options = pickerOptions(actionsBox = TRUE, maxOptions=1) )
+            
         ),
+        
+        
+            
 
         mainPanel(
           tabsetPanel(
-            tabPanel("Table",dataTableOutput("table_all"))
+            tabPanel("Table",dataTableOutput("table_all")),
+            tabPanel("Comparison", plotOutput("compare"))
            )
         )
     )
@@ -81,11 +132,29 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$table_all <- renderDataTable({
-      if(input$year==2020){data2020}
-      else if(input$year==2019){data2019}
-      else if(input$year==2018){data2018}
-      else if(input$year==2017){data2017}
-      else {data2016}
+      all_years_data%>%filter(year%in%input$year, university%in%input$uni, department%in%input$dept, city%in%input$city)
+    })
+    
+    selection_number <- renderText({
+      paste("You can only select a department and at most 5 universities to compare.")
+    })
+    
+    output$compare <- renderPlot({
+      if (input$ifcompare=="YES"){
+        data <- all_years_data %>% select(university, department, lowest_ranking, year) %>% 
+          filter(university%in%input$uni5, department%in%input$dept1)
+        ggplot(data,aes(x=year,y=lowest_ranking,color=university)) + 
+          geom_line(size=1.3) + scale_y_reverse() + 
+          labs(x="Year",y="Lowest Ranking",color="Universities") + 
+          theme_bw() + ggtitle("Lowest Ranking vs Department")
+        
+      } else {
+        shiny::showModal(modalDialog(
+          title = "Important message",
+          "You didn't want to campare results...!"
+        ))
+      }
+      
     })
 }
 
